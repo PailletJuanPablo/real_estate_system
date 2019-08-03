108e7c8583775b1fdf11475481322e25661440f5
PailletJuanPablo <pailletjuanpablo@gmail.com>
2019-08-03 05:42:45 -0300

    First commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,11 @@
+@extends('adminlte::page')
+
+@section('title', 'Oviedo BR')
+
+@section('content_header')
+    <h1>Oviedo Bienes Raíces</h1>
+@stop
+
+@section('content')
+    <p>Bienvenido</p>
+@stop
